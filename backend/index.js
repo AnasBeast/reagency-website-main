0a16b7009ceaa71b0b7965f997f4b8b08912a7fc
AnasBeast <boussehminea@gmail.com>
2023-06-11 20:08:49 +0100

    minor fixes

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,8 @@ app.get("/",(req,res)=>{
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Auth-Token");
     next();
 });
 app.use("/api/todos", todoRoutes);
